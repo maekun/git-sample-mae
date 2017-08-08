f6638f221075c96bda82dc0b46432cf6184c1c09
atsukoR <atsuko.yoshino@rakus.co.jp>
2017-08-08 15:52:44 +0900

    brake機能を追加しました！

--- src/main/java/jp/co/rakus/git_sample_mae/domain/Car.java
+++ src/main/java/jp/co/rakus/git_sample_mae/domain/Car.java
@@ -6,21 +6,23 @@ package jp.co.rakus.git_sample_mae.domain;
  */
 public class Car {
 
-	/** スピード*/
+	/** スピード */
 	private int speed;
-	
+
 	/** 車体の色 */
 	private String color;
 
-	
+	/** ブレーキ */
+	private int brake;
+
 	/**
 	 * 走ります.
 	 */
-	public void run(){
-		
+	public void run() {
+
 		this.speed += 150;
 	}
-	
+
 	public int getSpeed() {
 		return speed;
 	}
@@ -36,6 +38,13 @@ public class Car {
 	public void setColor(String color) {
 		this.color = color;
 	}
-	
-	
+
+	public int getBrake() {
+		return brake;
+	}
+
+	public void setBrake(int brake) {
+		this.brake = brake * 10;
+	}
+
 }
