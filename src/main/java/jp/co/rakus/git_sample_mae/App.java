716fd9fc11451d09eb7e0932bd2404a6edf456de
mae <hiroki.mae@rakus.co.jp>
2017-08-08 11:48:21 +0900

    新規にプロジェクトを作成#

---
+++ src/main/java/jp/co/rakus/git_sample_mae/App.java
@@ -0,0 +1,13 @@
+package jp.co.rakus.git_sample_mae;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
